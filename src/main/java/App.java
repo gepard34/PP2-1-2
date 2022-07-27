9ed5c06c07fd16b079648bb71ac31525552bfa5a
gepard34 <106550634+gepard34@users.noreply.github.com>
2022-07-27 13:34:01 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld") ;
         System.out.println(bean.getMessage());
+        System.out.println("Ссылки для Helloworld равны: " + (bean==bean2));
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Ссылки для Cat равны: " + (cat==cat1));
     }
 }
